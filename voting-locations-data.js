5a58d35b198868e3500e73a6847f931a9c023b9d
jgrepke5 <jonah@redrockstrategies.com>
2026-05-21 14:39:12 -0700

    Voting Location Upload

---
+++ voting-locations-data.js
@@ -0,0 +1,245 @@
+const votingLocations = [
+    {
+        "name": "Utilities Building",
+        "address": "34 Lakes Boulevard, Dayton, NV 89403",
+        "type": "early-only",
+        "lat": 39.2422078,
+        "lng": -119.5659914,
+        "schedule": [
+            {
+                "date": "Saturday (May 23)",
+                "hours": "10:00 a.m. to 3:00 p.m."
+            },
+            {
+                "date": "Sunday (May 24)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (May 25)",
+                "hours": "Closed for Memorial Day"
+            },
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "10:00 a.m. to 3:00 p.m."
+            },
+            {
+                "date": "Sunday (May 31)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (June 1)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Wednesday (June 3)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Thursday (June 4)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Friday (June 5)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            }
+        ],
+        "electionDay": null
+    },
+    {
+        "name": "Hillyard Hall Community Center",
+        "address": "2945 Ft. Churchill Street, Silver Springs 89429",
+        "type": "early-and-election",
+        "lat": 39.4060933,
+        "lng": -119.2252319,
+        "schedule": [
+            {
+                "date": "Saturday (May 23)",
+                "hours": "10:00 a.m. to 3:00 p.m."
+            },
+            {
+                "date": "Sunday (May 24)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (May 25)",
+                "hours": "Closed for Memorial Day"
+            },
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "10:00 a.m. to 3:00 p.m."
+            },
+            {
+                "date": "Sunday (May 31)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (June 1)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Wednesday (June 3)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Thursday (June 4)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            },
+            {
+                "date": "Friday (June 5)",
+                "hours": "9:00 a.m. to 5:00 p.m."
+            }
+        ],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "Douglas County Community Center",
+        "address": "1329 Waterloo Ln, Gardnerville, NV 89410",
+        "type": "early-and-election",
+        "lat": 38.9366761,
+        "lng": -119.7381601,
+        "schedule": [
+            {
+                "date": "Saturday (May 23)",
+                "hours": "10:00 a.m. to 2:00 p.m."
+            },
+            {
+                "date": "Sunday (May 24)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (May 25)",
+                "hours": "Closed for Memorial Day"
+            },
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "10:00 a.m. to 2:00 p.m."
+            },
+            {
+                "date": "Sunday (May 31)",
+                "hours": "Closed (Sunday)"
+            },
+            {
+                "date": "Monday (June 1)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Wednesday (June 3)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Thursday (June 4)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Friday (June 5)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "Indian Hills GID Offices",
+        "address": "3394 James Lee Park Rd., Carson City, NV 89705",
+        "type": "election-day-only",
+        "lat": 39.084689685383,
+        "lng": -119.784733093198,
+        "schedule": [],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "Kahle Community Center",
+        "address": "236 Kingsbury Grade Rd., Stateline, NV 89449",
+        "type": "election-day-only",
+        "lat": 38.968391984342,
+        "lng": -119.929089668495,
+        "schedule": [],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "TRE Community Center",
+        "address": "3939 Carter Drive., Wellington, NV 89444",
+        "type": "election-day-only",
+        "lat": 38.731805649908,
+        "lng": -119.504985396677,
+        "schedule": [],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "Washoe Tribe Community Center",
+        "address": "828 Me Hu St., Gardnerville, NV 89460",
+        "type": "election-day-only",
+        "lat": 38.898027070162,
+        "lng": -119.716846296696,
+        "schedule": [],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    },
+    {
+        "name": "Dayton High School Gym",
+        "address": "335 Dayton Valley Road, Dayton, NV 89403",
+        "type": "election-day-only",
+        "lat": 39.2421523,
+        "lng": -119.5694706,
+        "schedule": [],
+        "electionDay": "7:00 a.m. to 7:00 p.m."
+    }
+];
