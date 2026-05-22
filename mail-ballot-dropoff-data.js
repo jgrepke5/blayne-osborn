6ba774bb690dde48c70a881076c39a882c292279
jgrepke5 <jonah@redrockstrategies.com>
2026-05-21 17:25:27 -0700

    Add files via upload

---
+++ mail-ballot-dropoff-data.js
@@ -0,0 +1,199 @@
+const mailBallotDropoffLocations = [
+    {
+        "name": "Historic Courthouse",
+        "isNew": true,
+        "address": "1616 8th St, Minden, NV 89423",
+        "schedule": [
+            {
+                "date": "Saturday (May 23)",
+                "hours": "10:00 a.m. to 2:00 p.m."
+            },
+            {
+                "date": "Monday (May 25)",
+                "hours": "MEMORIAL DAY - NO DROP OFF"
+            },
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "10:00 a.m. to 2:00 p.m."
+            },
+            {
+                "date": "Monday (June 1)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Wednesday (June 3)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Thursday (June 4)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Friday (June 5)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Monday (June 8)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ],
+        "lat": 38.954664665279,
+        "lng": -119.768082689561
+    },
+    {
+        "name": "Fish Springs Fire Station",
+        "isNew": true,
+        "address": "2249 Fish Springs Road, Gardnerville, NV 89410",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            }
+        ],
+        "lat": 38.936178453589,
+        "lng": -119.656240918271
+    },
+    {
+        "name": "Gardnerville Ranchos Fire Station",
+        "isNew": true,
+        "address": "940 Mitch Dr, Gardnerville, NV 89460",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ],
+        "lat": 38.907487616225,
+        "lng": -119.73054065264
+    },
+    {
+        "name": "Genoa Town Hall",
+        "isNew": true,
+        "address": "2287 Main St, Genoa, NV, 89411",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ],
+        "lat": 39.002826034935,
+        "lng": -119.846118309641
+    },
+    {
+        "name": "Indian Hills GID Offices",
+        "isNew": false,
+        "address": "",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            }
+        ]
+    },
+    {
+        "name": "Johnson Lane Fire Station",
+        "isNew": true,
+        "address": "1450 Stephanie Way, Minden, NV 89423",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            },
+            {
+                "date": "Friday (May 29)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ],
+        "lat": 39.041283200555,
+        "lng": -119.732057272404
+    },
+    {
+        "name": "Kahle Community Center",
+        "isNew": false,
+        "address": "",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            },
+            {
+                "date": "Thursday (May 28)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Tuesday (June 2)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            }
+        ]
+    },
+    {
+        "name": "TRE Community Center",
+        "isNew": false,
+        "address": "",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            },
+            {
+                "date": "Wednesday (May 27)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            }
+        ]
+    },
+    {
+        "name": "Washoe Tribe Community Center",
+        "isNew": false,
+        "address": "",
+        "schedule": [
+            {
+                "date": "Tuesday (May 26)",
+                "hours": "9:00 a.m. to 6:00 p.m."
+            },
+            {
+                "date": "Saturday (May 30)",
+                "hours": "10:00 a.m. to 1:00 p.m."
+            }
+        ]
+    }
+];
